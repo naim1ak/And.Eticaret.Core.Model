70b750e26463d6ef7846545b097d4513ed4de0e0
naim ak <irecak@hotmail.com>
2019-08-20 05:16:34 +0300

    Add project files.

---
+++ And.Eticaret.UI.WEB/Areas/Admin/Controllers/AdminLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace And.Eticaret.UI.WEB.Areas.Admin.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        // GET: Admin/AdminLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
