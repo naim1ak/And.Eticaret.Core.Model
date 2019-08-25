0824b04f203acfeee521d6bdf77f9c7ba4d6694d
naim ak <irecak@hotmail.com>
2019-08-26 01:07:15 +0300

    Products eklendi

--- And.Eticaret.UI.WEB/Controllers/HomeController.cs
+++ And.Eticaret.UI.WEB/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using And.Eticaret.Core.Model;
+using And.Eticaret.Core.Model.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,15 @@ namespace And.Eticaret.UI.WEB.Controllers
 {
     public class HomeController : Controller
     {
+        AndDB db = new AndDB();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var data = db.Products.OrderByDescending(x => x.CreateDate).Take(5).ToList();
+            return View(data);
         }
         public PartialViewResult GetMenu()
         {
-            var db = new AndDB();
             var menus = db.Categories.Where(x => x.ParentID == 0).ToList();
             return PartialView(menus);
         }
@@ -46,6 +48,32 @@ namespace And.Eticaret.UI.WEB.Controllers
                 return View();
             }
 
+        }
+        [Route("Uye-Kayit")]
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("Uye-Kayit")]
+        public ActionResult CreateUser(User entity)
+        {
+            try
+            {
+                entity.CreateDate = DateTime.Now;
+                entity.CreateUserID = 1;
+                entity.IsActive = true;
+                entity.IsAdmin = false;
+
+                db.Users.Add(entity);
+                db.SaveChanges();
+                return Redirect("/");
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+            
         }
     }
 }
